1bd6e58c8dca44c6150f7753ad202f7d42bb1a03
Kamlesh Shirbhate <kshirbhate@netchexonline.com>
2018-01-14 10:54:02 +0530

    Hardcoded dashboard content added

--- MVCHackathon/MVCHackathon/Areas/Dashboard/Controllers/DashboardController.cs
+++ MVCHackathon/MVCHackathon/Areas/Dashboard/Controllers/DashboardController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using MVCHackathon.utilities;
+using MVCHackathon.Areas.Dashboard.Models;
 
 namespace MVCHackathon.Areas.Dashboard.Controllers
 {
@@ -13,7 +14,37 @@ namespace MVCHackathon.Areas.Dashboard.Controllers
         public ActionResult Index()
         {
             setupSession();
-            return View();
+            DashboardModel model = new DashboardModel();
+            DashboardModel OutgoingExternalList = new DashboardModel();
+            OutgoingExternalList.Daily = 4;
+            OutgoingExternalList.Weekly = 13;
+            OutgoingExternalList.Monthly = 56;
+            OutgoingExternalList.Yearly = 100;
+
+            model.OutgoingExternalList.Add(OutgoingExternalList);
+
+            DashboardModel OutgoingInternalList = new DashboardModel();
+            OutgoingInternalList.Daily = 11;
+            OutgoingInternalList.Weekly = 23;
+            OutgoingInternalList.Monthly = 66;
+            OutgoingInternalList.Yearly = 260;
+            model.OutgoingInternalList.Add(OutgoingInternalList);
+
+            DashboardModel IncomingExternalList = new DashboardModel();
+            IncomingExternalList.Daily = 34;
+            IncomingExternalList.Weekly = 67;
+            IncomingExternalList.Monthly = 100;
+            IncomingExternalList.Yearly = 647;
+            model.IncomingExternalList.Add(IncomingExternalList);
+
+            DashboardModel IncomingInternalList = new DashboardModel();
+            IncomingInternalList.Daily = 62;
+            IncomingInternalList.Weekly = 240;
+            IncomingInternalList.Monthly = 567;
+            IncomingInternalList.Yearly = 890;
+            model.IncomingInternalList.Add(IncomingInternalList);
+
+            return View(model);
         }
     }
 }
---
+++ MVCHackathon/MVCHackathon/Areas/Dashboard/Models/DashboardModel.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using MVCHackathon.Areas.Customer.Models;
+
+namespace MVCHackathon.Areas.Dashboard.Models
+{
+    public class DashboardModel : CustomerModel
+    {
+        private long _Daily;
+        private long _Weekly;
+        private long _Monthly;
+        private long _Yearly;
+        private List<DashboardModel> _OutgoingExternalList = new List<DashboardModel>();
+        private List<DashboardModel> _OutgoingInternalList = new List<DashboardModel>();
+        private List<DashboardModel> _IncomingExternalList = new List<DashboardModel>();
+        private List<DashboardModel> _IncomingInternalList = new List<DashboardModel>();
+        private List<DashboardModel> _List = null;
+
+        public long Daily
+        {
+            get
+            {
+                return _Daily;
+            }
+
+            set
+            {
+                _Daily = value;
+            }
+        }
+
+        public List<DashboardModel> List
+        {
+            get
+            {
+                if (_List == null) _List = new List<DashboardModel>();
+                return _List;
+            }
+
+            set
+            {
+                _List = value;
+            }
+        }
+
+        public long Weekly
+        {
+            get
+            {
+                return _Weekly;
+            }
+
+            set
+            {
+                _Weekly = value;
+            }
+        }
+
+        public long Monthly
+        {
+            get
+            {
+                return _Monthly;
+            }
+
+            set
+            {
+                _Monthly = value;
+            }
+        }
+
+        public long Yearly
+        {
+            get
+            {
+                return _Yearly;
+            }
+
+            set
+            {
+                _Yearly = value;
+            }
+        }
+
+        public List<DashboardModel> OutgoingExternalList
+        {
+            get
+            {
+                if (_OutgoingExternalList == null) _OutgoingExternalList = new List<DashboardModel>();
+                return _OutgoingExternalList;
+            }
+
+            set
+            {
+                _OutgoingExternalList = value;
+            }
+        }
+
+        public List<DashboardModel> OutgoingInternalList
+        {
+            get
+            {
+                if (_OutgoingInternalList == null) _OutgoingInternalList = new List<DashboardModel>();
+                return _OutgoingInternalList;
+            }
+
+            set
+            {
+                _OutgoingInternalList = value;
+            }
+        }
+
+        public List<DashboardModel> IncomingExternalList
+        {
+            get
+            {
+                if (_IncomingExternalList == null) _IncomingExternalList = new List<DashboardModel>();
+                return _IncomingExternalList;
+            }
+
+            set
+            {
+                _IncomingExternalList = value;
+            }
+        }
+
+        public List<DashboardModel> IncomingInternalList
+        {
+            get
+            {
+                if (_IncomingInternalList == null) _IncomingInternalList = new List<DashboardModel>();
+                return _IncomingInternalList;
+            }
+
+            set
+            {
+                _IncomingInternalList = value;
+            }
+        }
+    }
+}
